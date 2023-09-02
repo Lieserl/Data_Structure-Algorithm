dc39bff288daf64b9f53cce7e76185aa935de88b
Lieserl <HYuhang@bupt.edu.cn>
2023-09-02 13:31:00 +0800

    main

---
+++ Data Structure/Disjoint Set Union/Acwing 837.  The Number of Points in a connected component.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+using namespace std;
+
+const int N = 1e5 + 10;
+
+int n, m;
+int p[N], siz[N];
+
+int find(int x)
+{
+    if (p[x] != x)
+        p[x] = find(p[x]);
+    return p[x];
+}
+
+int main()
+{
+    cin >> n >> m;
+
+    // Initialize
+    for (int i = 1; i <= n; i++)
+    {
+        p[i] = i;
+        siz[i] = 1;
+    }
+
+    while (m--)
+    {
+        string op;
+        int a, b;
+        cin >> op;
+        if (op[0] == 'C')
+        {
+            cin >> a >> b;
+            if (find(a) == find(b))
+                continue;
+            siz[find(b)] += siz[find(a)];
+            p[find(a)] = find(b);
+                }
+
+        else if (op[1] == '1')
+        {
+            cin >> a >> b;
+            if (find(a) == find(b))
+                cout << "Yes" << endl;
+            else
+                cout << "No" << endl;
+        }
+
+        else
+        {
+            cin >> a;
+            cout << siz[find(a)] << endl;
+        }
+    }
+
+    return 0;
+}
---
+++ Data Structure/Disjoint Set Union/README.md
@@ -0,0 +1,41 @@
+# Basic Knowledge about Disjoint-Set Union
+
+## Fundamental Principles of Disjoint-Set Union
+
+Each set is represented by a tree, where the root's label is the identifier for the entire set, and each node stores its parent node, and *p[x]* represent the parent node of *x*.
+
+### Question
+* How to determine if a node is the root:
+`if (p[x] == x)`
+* How to find the set identifier for x:
+`while (p[x] != x) x = p[x]`
+* How to merge two sets together: Assuming x, y are identifiers of two sets.
+`p[x] = y`
+
+## Operations supported by Disjoint-Set Union
+
+### Initialization
+Initially, each element is a separate set.
+`for (int i = 1; i <= n; i++) p[i] = i`
+
+### Find
+```
+int find(int x)
+{
+    if (p[x] != x) p[x] = find(p[x])
+    return p[x];
+}
+```
+
+### Merge
+`p[find(a)] = find(b);`
+
+Find and Merge operations are in nearly *O(1)* time complexity due to *Path Compression*
+
+## Path Compression
+After a find operation, for a path from *x* to root, directly point them to the root.
+`x = find(p[x]);`
+
+
+
+
